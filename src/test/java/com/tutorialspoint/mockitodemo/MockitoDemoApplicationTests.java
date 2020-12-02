8d7001bdac319da1c2ccca38d3a07af1aca05181
古川広一 <ex.kouichi1224@gmail.com>
2020-12-03 00:29:07 +0900

    first commit

---
+++ src/test/java/com/tutorialspoint/mockitodemo/MockitoDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		fail("これは失敗するはず");
+	}
+
+}
