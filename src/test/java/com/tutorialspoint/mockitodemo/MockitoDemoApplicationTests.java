38d721445108ac32f18decba5dd6d1bffdf15200
古川広一 <ex.kouichi1224@gmail.com>
2020-12-03 21:47:10 +0900

    lombokの導入

--- src/test/java/com/tutorialspoint/mockitodemo/MockitoDemoApplicationTests.java
+++ src/test/java/com/tutorialspoint/mockitodemo/MockitoDemoApplicationTests.java
@@ -2,9 +2,7 @@ package com.tutorialspoint.mockitodemo;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MockitoDemoApplicationTests {
 
 	@Test
