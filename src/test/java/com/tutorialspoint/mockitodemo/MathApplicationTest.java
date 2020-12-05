6b7353b56d3e9f239b132382d11b6e262e6a5c64
古川広一 <ex.kouichi1224@gmail.com>
2020-12-05 18:54:24 +0900

    add: Mockito - Exception Handling

--- src/test/java/com/tutorialspoint/mockitodemo/MathApplicationTest.java
+++ src/test/java/com/tutorialspoint/mockitodemo/MathApplicationTest.java
@@ -23,28 +23,14 @@ class MathApplicationTest {
     private CalculatorService calcService;
 
     @Test
-    public void test_2つの値を加算する() {
-        // add the behavior of calc service to add two numbers
-        when(calcService.add(10.0,20.0)).thenReturn(30.00);
-
-        // add the behavior of calc service to subtract two numbers
-        when(calcService.subtract(20.0, 10.0)).thenReturn(10.00);
+    public void test_addメソッドを呼び出すと例外処理となる() {
+        // add the behavior to throw exception
+        doThrow(new RuntimeException("Add operation not implemented"))
+                .when(calcService).add(10.0, 20.0);
 
         // test the add functionality
-        assertEquals(calcService.add(10.0, 20.0),30.0,0);
-        assertEquals(calcService.add(10.0, 20.0),30.0,0);
-        assertEquals(calcService.add(10.0, 20.0),30.0,0);
-
-        // test the subtract functionality
-        assertEquals(calcService.subtract(20.0, 10.0),10.0,0);
-
-        // check a minimum 1 call count
-        verify(calcService, atLeastOnce()).subtract(20.0, 10.0);
-
-        // check if add function is called minimum 2 times
-        verify(calcService, atLeast(2)).add(10.0, 20.0);
-
-        // check if add function is called maximum 3 times
-        verify(calcService, atMost(3)).add(10.0, 20.0);
+        assertThrows(RuntimeException.class, () -> {
+            calcService.add(10.0, 20.0);
+        });
     }
 }
